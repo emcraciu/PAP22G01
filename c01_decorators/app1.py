13a09a902b6853c085638490f9b6048f96cff43a
emanuel.craciun <emanuel.craciun@savnet.ro>
2022-03-21 18:36:33 +0200

    draft

--- c01_decorators/app1.py
+++ c01_decorators/app1.py
@@ -4,20 +4,24 @@ import time
 from datetime import datetime
 from functools import wraps
 
+
 def decorator(func):
     @wraps(func)
     def wrapper(*args, **kwargs):
         start = datetime.now()
         functie = func(*args, **kwargs)
         end = datetime.now()
-        print(f"Execution time: {(end-start).microseconds}")
+        print(f"Execution time: {(end - start).microseconds}")
         return functie
+
     return wrapper
+
 
 @decorator
 def chr_count(message):
     for _ in message:
         time.sleep(0.1)
     return len(message)
+
 
 print(chr_count('message'))
---
+++ c2_property_regex/homework.md
@@ -0,0 +1,7 @@
+## Quiz
+https://www.classmarker.com/
+
+## Homework
+Extend existing functionality in of app1 class SystemInformation to support
+ - getting available memory (total, used, free)
+ - getting routing table and displaying it as a table
